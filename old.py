7385c62808a2b61029787ab6c02c48f0799156b8
Your Name <davidriazati@fb.com>
2020-05-15 13:36:45 -0700

    more

--- old.py
+++ old.py
@@ -6,6 +6,9 @@ import random
 import numpy as np
 from sklearn.cluster import KMeans
 from geopy.distance import great_circle
+# import matplotlib.pyplot as plt
+# from sklearn.cluster import DBSCAN
+# from shapely.geometry import MultiPoint
 
 from helpers import *
 
@@ -13,11 +16,13 @@ Coordinates = collections.namedtuple("Coordinates", ["lat", "lon"])
 
 
 # Parameters
-MIN_DELIVERY_WINDOW_DAYS = 30
+MIN_DELIVERY_WINDOW = 30
+GROUP_SIZE = 5
 STATE = "GA"
 CITY = "atlanta"
 ADDRESSES_FILE = "file.csv"
 FIX_MISSING_ADDRESSES = False
+ITERATIONS = 500
 
 
 addresses = pickle.load(open("addresses.pkl", "rb"))
@@ -26,13 +31,10 @@ num_bad_addresses = 0
 num_total = 0
 num_with_recent_deliveries = 0
 
-# This is the list of all the people that will get deliveries
 requesters = []
-
-# Read the CSV file and get a list of people requesting deliveries
 for row in csv.DictReader(open(ADDRESSES_FILE, "r")):
     num_total += 1
-    if int(row["Days since delivery"]) < MIN_DELIVERY_WINDOW_DAYS:
+    if int(row["Days since delivery"]) < MIN_DELIVERY_WINDOW:
         # Skip people who have had deliveries in the last month
         num_with_recent_deliveries += 1
         continue
@@ -60,8 +62,6 @@ for row in csv.DictReader(open(ADDRESSES_FILE, "r")):
         {"row": row, "coords": coords}
     )
 
-
-# Store the addressess so we don't have to fetch them from Bing every time
 pickle.dump(addresses, open("addresses.pkl", "wb"))
 
 print(" ======= stats =======")
@@ -74,8 +74,18 @@ print("skipped (address was bad):", num_bad_addresses)
 print("calculating routes for:", len(requesters))
 
 
-# Run kmeans to find clusters of people
-locations_by_idx = [[r["coords"].lat, r["coords"].lon] for r in requesters]
+locations_by_idx = []
+lats = []
+lons = []
+
+
+for r in requesters:
+    locations_by_idx.append([r["coords"].lat, r["coords"].lon])
+    lats.append(r["coords"].lat)
+    lons.append(r["coords"].lon)
+
+
+# Run kmeans
 num_clusters = int(len(requesters) / GROUP_SIZE)
 kmeans = KMeans(n_clusters=num_clusters, random_state=0).fit(np.array(locations_by_idx))
 
@@ -85,32 +95,116 @@ for index, group_index in enumerate(kmeans.labels_):
     groups[group_index].append(index)
 
 
-# Print out groups
+# some of the groups at this point are too big and need to be split up
+# groups = []
+# for potential_group in potential_groups:
+#     if len(potential_group) <= GROUP_SIZE:
+#         groups.append(potential_group)
+#     else:
+#         curr = 0
+#         while curr < len(potential_group):
+#             partial_group = potential_group[curr:curr + GROUP_SIZE]
+#             groups.append(partial_group)
+#             curr += GROUP_SIZE
+
+
+
+def make_colors():
+    for group_idx, group in enumerate(groups):
+        color = "#%02X%02X%02X" % (
+            random.randint(0, 255),
+            random.randint(0, 255),
+            random.randint(0, 255),
+        )
+        for x in group:
+            requesters[x]["color"] = color
+            requesters[x]["group_idx"] = group_idx
+
+    colors = []
+    for r in requesters:
+        colors.append(r["color"])
+    return colors
+
+
+def make_group_colors(group_idx):
+    for r in requesters:
+        if r["group_idx"] != group_idx:
+            r["orig_color"] = r["color"]
+            r["color"] = (0, 0, 0, 0.1)
+
+    colors = []
+    for r in requesters:
+        colors.append(r["color"])
+    return colors
+
+
+def revert_colors():
+    for r in requesters:
+        if "orig_color" in r:
+            r["color"] = r["orig_color"]
+            del r["orig_color"]
+
+    colors = []
+    for r in requesters:
+        colors.append(r["color"])
+    return colors
+
+
+# fig, ax = plt.subplots()
+# coll = ax.scatter(lats, lons, c=make_colors(), picker=0.01)
+# artist = None
+
+
+def on_pick(ev):
+    r = requesters[ev.ind[0]]
+    pprint.pprint(r["row"]["Computed Address"])
+    ev.artist.set_facecolors(make_group_colors(r["group_idx"]))
+    global artist
+    artist = ev.artist
+    fig.canvas.draw()
+
+
+def press(event):
+    if event.key == "x":
+        artist.set_facecolors(revert_colors())
+        fig.canvas.draw()
+
+
+# fig.canvas.callbacks.connect("pick_event", on_pick)
+# fig.canvas.mpl_connect("key_press_event", press)
+
+# plt.show()
+
+def average_coords(coords):
+    avg_lat = 0
+    avg_lon = 0
+    for c in coords:
+        avg_lat += c.lat
+        avg_lon += c.lon
+    avg_lat = avg_lat / len(coords)
+    avg_lon = avg_lon / len(coords)
+
+    return avg_lat, avg_lon
+
+
 for group in groups:
     waypoints = [requesters[i]['coords'] for i in group]
     if len(waypoints) > 20:
         print("Skipping group that is too big")
         continue
-
-    # Find the drive time for the whole route
     travel_time_minutes, data = driving_distance(waypoints)
     print('\n')
 
     print(f"Driving distance: {round(travel_time_minutes, 2)} min")
 
-    # Finding the driving distance optimizes the order of the destinations, so
-    # use that
     if 'waypointsOrder' not in data:
         order = [i for i in range(len(group))]
     else:
         order = [int(x.replace('wp.', '')) for x in data['waypointsOrder']]
-
     for i, ordered_index in enumerate(order):
         idx = group[ordered_index]
         r = requesters[idx]
-        # Show the address
         print(f"{i}. {r['row']['Computed Address']}")
         if travel_time_minutes > 0 and i < len(order) - 1:
-            # Show the driving time from this address to the next one in the list
             travel_time_from_last_waypoint_minutes = data['routeLegs'][i]['travelDuration'] / 60
             print(f"\t{round(travel_time_from_last_waypoint_minutes, 2)} minutes")
