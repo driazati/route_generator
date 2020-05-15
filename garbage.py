4597e344b087690d0e660f1297679ad469800018
Your Name <davidriazati@fb.com>
2020-05-15 13:08:14 -0700

    more

--- garbage.py
+++ garbage.py
@@ -63,3 +63,99 @@ def kmeans():
         groups[index].append(i)
         i += 1
     return groups
+
+
+
+
+
+
+
+# calculate distance matrix
+# print("calculating distances...")
+# for r in requesters:
+#     r['distances'] = []
+#     for other_r in requesters:
+#         r["distances"].append(great_circle(r["coords"], other_r["coords"]).km)
+# print("\tdone")
+
+
+# def group_requesters():
+#     groups = []
+#     indices = [i for i in range(len(requesters))]
+#     random.shuffle(indices)
+#     for r in requesters:
+#         r["visited"] = False
+
+#     for this_index in indices:
+#         r = requesters[this_index]
+#         if r["visited"]:
+#             continue
+#         r["visited"] = True
+
+#         group = [this_index]
+#         center = [r["coords"].lat, r["coords"].lon]
+#         for i in range(GROUP_SIZE - 1):
+#             # find the closest non-visited neighbor, add them to the group with this one,
+#             # and mark it visited
+#             min_distance = 9999999999999
+#             min_requester_index = -1
+#             for other_index, other_dist in enumerate(requesters):
+#                 other_r = requesters[other_index]
+#                 if other_r["visited"]:
+#                     # Skip visited
+#                     continue
+#                 dist = r["distances"][other_index]
+#                 if dist < min_distance:
+#                     min_distance = dist
+#                     min_requester_index = other_index
+
+#             if min_requester_index != -1:
+#                 item = requesters[min_requester_index]
+#                 center[0] = (center[0] * len(group) + item["coords"].lat) / (
+#                     len(group) + 1
+#                 )
+#                 center[1] = (center[1] * len(group) + item["coords"].lon) / (
+#                     len(group) + 1
+#                 )
+#                 group.append(min_requester_index)
+#                 item["visited"] = True
+#         groups.append(group)
+#     return groups
+
+
+# def rate(groups):
+#     total_distance = 0
+#     for group in groups:
+#         avg_lat = 0
+#         avg_lon = 0
+#         for x in group:
+#             avg_lat += requesters[x]["coords"].lat
+#             avg_lon += requesters[x]["coords"].lon
+#         avg_lat = avg_lat / len(group)
+#         avg_lon = avg_lon / len(group)
+
+#         for x in group:
+#             lat = requesters[x]["coords"].lat
+#             lon = requesters[x]["coords"].lon
+#             dist = great_circle([lat, lon], [avg_lat, avg_lon])
+#             total_distance += dist.km
+
+#     return total_distance
+
+
+# # Randomly choose a set of groupings and rate it
+# best_groups = None
+# min_score = None
+# for i in range(ITERATIONS):
+#     print("Iteration:", i)
+#     groups = group_requesters()
+#     if best_groups is None:
+#         best_groups = groups
+#         min_score = rate(groups)
+#     else:
+#         score = rate(groups)
+#         if score < min_score:
+#             print("\tFound new minimum")
+#             min_score = score
+#             best_groups = groups
+#     print("\tMin score: ", min_score)
--- main.py
+++ main.py
@@ -73,99 +73,6 @@ print(
 print("skipped (address was bad):", num_bad_addresses)
 print("calculating routes for:", len(requesters))
 
-# calculate distance matrix
-# print("calculating distances...")
-# for r in requesters:
-#     r['distances'] = []
-#     for other_r in requesters:
-#         r["distances"].append(great_circle(r["coords"], other_r["coords"]).km)
-# print("\tdone")
-
-
-# def group_requesters():
-#     groups = []
-#     indices = [i for i in range(len(requesters))]
-#     random.shuffle(indices)
-#     for r in requesters:
-#         r["visited"] = False
-
-#     for this_index in indices:
-#         r = requesters[this_index]
-#         if r["visited"]:
-#             continue
-#         r["visited"] = True
-
-#         group = [this_index]
-#         center = [r["coords"].lat, r["coords"].lon]
-#         for i in range(GROUP_SIZE - 1):
-#             # find the closest non-visited neighbor, add them to the group with this one,
-#             # and mark it visited
-#             min_distance = 9999999999999
-#             min_requester_index = -1
-#             for other_index, other_dist in enumerate(requesters):
-#                 other_r = requesters[other_index]
-#                 if other_r["visited"]:
-#                     # Skip visited
-#                     continue
-#                 dist = r["distances"][other_index]
-#                 if dist < min_distance:
-#                     min_distance = dist
-#                     min_requester_index = other_index
-
-#             if min_requester_index != -1:
-#                 item = requesters[min_requester_index]
-#                 center[0] = (center[0] * len(group) + item["coords"].lat) / (
-#                     len(group) + 1
-#                 )
-#                 center[1] = (center[1] * len(group) + item["coords"].lon) / (
-#                     len(group) + 1
-#                 )
-#                 group.append(min_requester_index)
-#                 item["visited"] = True
-#         groups.append(group)
-#     return groups
-
-
-# def rate(groups):
-#     total_distance = 0
-#     for group in groups:
-#         avg_lat = 0
-#         avg_lon = 0
-#         for x in group:
-#             avg_lat += requesters[x]["coords"].lat
-#             avg_lon += requesters[x]["coords"].lon
-#         avg_lat = avg_lat / len(group)
-#         avg_lon = avg_lon / len(group)
-
-#         for x in group:
-#             lat = requesters[x]["coords"].lat
-#             lon = requesters[x]["coords"].lon
-#             dist = great_circle([lat, lon], [avg_lat, avg_lon])
-#             total_distance += dist.km
-
-#     return total_distance
-
-
-# # Randomly choose a set of groupings and rate it
-# best_groups = None
-# min_score = None
-# for i in range(ITERATIONS):
-#     print("Iteration:", i)
-#     groups = group_requesters()
-#     if best_groups is None:
-#         best_groups = groups
-#         min_score = rate(groups)
-#     else:
-#         score = rate(groups)
-#         if score < min_score:
-#             print("\tFound new minimum")
-#             min_score = score
-#             best_groups = groups
-#     print("\tMin score: ", min_score)
-
-
-
-
 
 locations_by_idx = []
 lats = []
@@ -182,28 +89,23 @@ for r in requesters:
 num_clusters = int(len(requesters) / GROUP_SIZE)
 kmeans = KMeans(n_clusters=num_clusters, random_state=0).fit(np.array(locations_by_idx))
 
-potential_groups = [[] for i in range(num_clusters)]
+groups = [[] for i in range(num_clusters)]
 
 for index, group_index in enumerate(kmeans.labels_):
-    potential_groups[group_index].append(index)
+    groups[group_index].append(index)
 
 
 # some of the groups at this point are too big and need to be split up
-groups = []
-for potential_group in potential_groups:
-    if len(potential_group) <= GROUP_SIZE:
-        groups.append(potential_group)
-    else:
-        curr = 0
-        while curr < len(potential_group):
-            partial_group = potential_group[curr:curr + GROUP_SIZE]
-            groups.append(partial_group)
-            curr += GROUP_SIZE
-
-
-
-# dont split the groups
-# groups = potential_groups
+# groups = []
+# for potential_group in potential_groups:
+#     if len(potential_group) <= GROUP_SIZE:
+#         groups.append(potential_group)
+#     else:
+#         curr = 0
+#         while curr < len(potential_group):
+#             partial_group = potential_group[curr:curr + GROUP_SIZE]
+#             groups.append(partial_group)
+#             curr += GROUP_SIZE
 
 
 
